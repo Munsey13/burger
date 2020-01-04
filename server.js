b9e563c11f28c082d3108939ca7115b409bacaac
MM13 <Fastsoccer13@gmail.com>
2020-01-03 19:00:24 -0500

    added routes that will require burgers_controller.js and app.use with routes parameter

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
+let routes = require("./controllers/burgers_controller");
+app.use(routes);
+
 //Starting server to listen for client reuests
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost: " + PORT);
