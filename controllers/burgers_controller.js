576c8f059c915552cd8a1a4610ab3817f0935f3c
MM13 <Fastsoccer13@gmail.com>
2020-02-28 20:02:47 -0500

    change some of the router functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,44 +3,35 @@ const router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get("/", function (req, res) {
-    res.redirect("/burgers");
-})
 
+    
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.all(function(burgerData) {
         let hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+        console.log(burgerData);
+        res.render("index", { burger_data:
+        burgerDats });
     });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/burgers/create", function(req, res) {
     burger.create([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, false
+        req.body.burger_name,
     ], function(result) {
-        res.json({ id: result.insertId});
+        res.redirect("/");
     });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-    let condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
-    burger.update ({
-        devoured: req.body.devoured
-    }, condition, function(result) {
-        if (result.changedRows === 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+router.put("/burgers/:id", function(req, res) {
+   
+    burger.update(req.params.id, function(result) {
+        
+        console.log(result);
+       
+        res.sendStatus(200);
+      });
     });
-});
 
 module.exports = router;
